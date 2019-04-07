73e077a04a99679dc1e6bf2821e994f609d8c3b4
Mehmet YILDIZ <mehmet@yildizmehmet.net>
2019-04-07 16:42:06 +0300

    Added MVC support to CartWebAPI

---
+++ CartWebAPI/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿namespace CartWebAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("[controller]")]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
--- CartWebAPI/Startup.cs
+++ CartWebAPI/Startup.cs
@@ -2,7 +2,7 @@
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.DependencyInjection;
 
 #pragma warning disable S2325 // Methods and properties that don't access instance data should be static
@@ -12,6 +12,7 @@
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +23,7 @@
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 #pragma warning restore S2325 // Methods and properties that don't access instance data should be static
