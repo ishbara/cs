a4a382b9249780058df2b03204b42bd597fef6ce
Mehmet YILDIZ <mehmet@yildizmehmet.net>
2019-04-09 13:46:27 +0300

    docker support added

---
+++ build.ps1
@@ -0,0 +1,4 @@
+﻿dotnet restore CartWebAPI
+dotnet publish -c Release -o ../publish/CartWebAPI CartWebAPI/CartWebAPI.csproj
+copy docker-conf/redisSettings.json -Destination publish/CartWebAPI/redisSettings.json
+docker build -t cart-api publish/CartWebAPI
